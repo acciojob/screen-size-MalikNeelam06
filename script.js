2c7738e1d243da878e67b717bd118bd09675bb80
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-26 22:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let button1=document.getElementById("open");
 let button2=document.getElementById("newwindow");
 let newwindow1;
 button1.addEventListener("click",()=>{
-	newwindow1=window.open("","newwindow","width=300,height=200");
+	newwindow1=window.open("","newwindow","width=1000,height=200");
 });
 button2.addEventListener("click",()=>{
-	newwindow1.resizeBy(665,359);
+	newwindow1.resizeBy(600,359);
 	newwindow1.focus();
 });
